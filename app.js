a07056a09c0fe7f034b6d4123d57922e47e65cd7
Ashok <ashok@codecraft.co.in>
2017-12-20 16:00:00 +0530

    CORS Allowed

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/api', mainController);
 
+app.use(function (req, res, next) {	
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization ,Token, Content-Type, Accept");
+    next();
+    });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
